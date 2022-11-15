3c7ea6c1b639180677d81c62b7041e5b6a112aa0
Dominic Moser <59155488+Dominic-Moser@users.noreply.github.com>
2022-11-15 14:46:40 -0700

    Day 2
    As said - day 2

---
+++ Day 2/PlanetProject.js
@@ -0,0 +1,11 @@
+function terraForm()
+{
+//     const bluePlanet = "https://c.tadst.com/gfx/1200x630/neptune.jpg?1";
+//     const greenPlanet = "https://pixy.org/download/373669/";
+//     const redPlanet = "https://as2.ftcdn.net/v2/jpg/02/44/98/75/1000_F_244987574_7WXXaMrlHCbW3IYyercYOWUc8XVprEwp.jpg";
+    const speech = document.getElementById("speech");
+
+    redPlanet.src = "https://pixy.org/download/373669/";
+
+    speech.style.color = red;
+}
