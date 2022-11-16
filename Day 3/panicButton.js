778a004425c403f0d2bd364c2df49126371376b2
Dominic Moser <59155488+Dominic-Moser@users.noreply.github.com>
2022-11-16 14:46:30 -0700

    Panic Button Down

---
+++ Day 3/panicButton.js
@@ -0,0 +1,13 @@
+function soothingMessage()
+{
+
+    if(document.getElementById("message").innerHTML == "It's gon' be alright.")
+    {
+        document.getElementById("message").innerHTML = ""
+
+    }
+    else{
+    document.getElementById("message").innerHTML = "It's gon' be alright.";
+
+    }
+}
