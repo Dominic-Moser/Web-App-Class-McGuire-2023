424ffa257ae53928f6aa5eb9eeb9284d614a38bc
Wambatly <59155488+Wambatly@users.noreply.github.com>
2022-11-25 14:41:15 -0700

    Did alot of suff, maybe too much
    asdasdsadas

--- Day 3/panicButton.js
+++ Day 3/panicButton.js
@@ -1,12 +1,18 @@
-function soothingMessage()
-{
-    if(page.style.animationName != "newPage")
-    {
-        page.style.animationName = "newPage";
-    }
-     else if(page.style.animationName == "newPage")
-    {
-        page.style.animationName = "newPageReverse";
-    }
+// function soothingMessage()
+// {
+//     if(page.style.animationName != "newPage")
+//     {
+//         page.style.animationName = "newPage";
+//     }
+//      else if(page.style.animationName == "newPage")
+//     {
+//         page.style.animationName = "newPageReverse";
+//     }
 
+// }
+function soothingMessage() {
+    page.classList.toggle("open");
+  }
+function soothingMessage2() {
+  page2.classList.toggle("open");
 }
